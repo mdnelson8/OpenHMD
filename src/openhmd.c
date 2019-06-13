mdnelson8/OpenHMD

commit 2be3730d62aeecb82663ec4c5f82c2784340a923

potential fix for rotation + translation order

src/openhmd.c
@@ -288,7 +288,7 @@ static int ohmd_device_getf_unp(ohmd_device* device, ohmd_float_value type, floa
 			mat4x4f orient, world_shift, result;
 			omat4x4f_init_look_at(&orient, &rot, &point);
 			omat4x4f_init_translate(&world_shift, -device->position.x +(device->properties.ipd / 2.0f), -device->position.y, -device->position.z);
-			omat4x4f_mult(&world_shift, &orient, &result);
+			omat4x4f_mult(&orient, &world_shift, &result);
 			omat4x4f_transpose(&result, (mat4x4f*)out);
 			return OHMD_S_OK;
 		}
@@ -299,7 +299,7 @@ static int ohmd_device_getf_unp(ohmd_device* device, ohmd_float_value type, floa
 			mat4x4f orient, world_shift, result;
 			omat4x4f_init_look_at(&orient, &rot, &point);
 			omat4x4f_init_translate(&world_shift, -device->position.x + -(device->properties.ipd / 2.0f), -device->position.y, -device->position.z);
-			omat4x4f_mult(&world_shift, &orient, &result);
+			omat4x4f_mult(&orient, &world_shift, &result);
 			omat4x4f_transpose(&result, (mat4x4f*)out);
 			return OHMD_S_OK;
 		}

commit 3904607dd9ce041d18436498b246552973c2de93

potential fix for rotation + translation order v2
Fixes: 2be3730d62ae ("potential fix for rotation + translation order")

src/openhmd.c
@@ -283,24 +283,22 @@ static int ohmd_device_getf_unp(ohmd_device* device, ohmd_float_value type, floa
 {
 	switch(type){
 	case OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX: {
-			vec3f point = {{0, 0, 0}};
 			quatf rot = device->rotation;
 			oquatf_mult_me(&rot, &device->rotation_correction);
-			mat4x4f orient, world_shift, result;
-			omat4x4f_init_look_at(&orient, &rot, &point);
-			omat4x4f_init_translate(&world_shift, -device->position.x +(device->properties.ipd / 2.0f), -device->position.y, -device->position.z);
-			omat4x4f_mult(&orient, &world_shift, &result);
+			mat4x4f central_view, eye_shift, result;
+			omat4x4f_init_look_at(&central_view, &rot, &device->position);
+			omat4x4f_init_translate(&eye_shift, +(device->properties.ipd / 2.0f), 0.0f, 0.0f);
+			omat4x4f_mult(&eye_shift, &central_view, &result);
 			omat4x4f_transpose(&result, (mat4x4f*)out);
 			return OHMD_S_OK;
 		}
 	case OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX: {
-			vec3f point = {{0, 0, 0}};
 			quatf rot = device->rotation;
 			oquatf_mult_me(&rot, &device->rotation_correction);
-			mat4x4f orient, world_shift, result;
-			omat4x4f_init_look_at(&orient, &rot, &point);
-			omat4x4f_init_translate(&world_shift, -device->position.x + -(device->properties.ipd / 2.0f), -device->position.y, -device->position.z);
-			omat4x4f_mult(&orient, &world_shift, &result);
+			mat4x4f central_view, eye_shift, result;
+			omat4x4f_init_look_at(&central_view, &rot, &device->position);
+			omat4x4f_init_translate(&eye_shift, -(device->properties.ipd / 2.0f), 0.0f, 0.0f);
+			omat4x4f_mult(&eye_shift, &central_view, &result);
 			omat4x4f_transpose(&result, (mat4x4f*)out);
 			return OHMD_S_OK;
 		}